d930fee5bfafc3dcbce17fe228d5db48f9741957
Ako Kaman <sheperson@gmail.com>
2015-01-13 01:01:24 +0330

    Add findOrCreateContainer function

--- js/growl.js
+++ js/growl.js
@@ -1,2 +1,17 @@
 var Growl = (function() {
+  // Look for the container element, and if it is not found, create it!
+  var findOrCreateContainer = function() {
+    // Try finding the container element first
+    var growlContainer = document.getElementById("growl-container");
+
+    // If we can not find it, let's create it
+    if (!growlContainer) {
+      growlContainer= document.createElement("div");
+      growlContainer.id = "growl-container";
+      growlContainer.className = "growl-container";
+      document.body.appendChild(growlContainer);
+    }
+
+    return growlContainer;
+  };
 })(this);
