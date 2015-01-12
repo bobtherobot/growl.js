dbfc540ab75a4b66ec974fccf4f9af1a48c507f1
Ako Kaman <sheperson@gmail.com>
2015-01-13 01:02:56 +0330

    Add the show function

--- js/growl.js
+++ js/growl.js
@@ -14,4 +14,47 @@ var Growl = (function() {
 
     return growlContainer;
   };
+
+  // Main function which does the heavy lifting
+  var show = function(args) {
+    var growl, message, title, growlContainer, timeoutHandle;
+
+    // Set default arguments, if they are missing
+    args.message = args.message || "";
+    args.type = args.type || "notice";
+    args.timeout = args.timeout || 2500;
+
+    // Create the growl message
+    growl = document.createElement("div");
+    message = document.createTextNode(args.message);
+
+    // If there is a title specified, create it
+    if (args.title && typeof(args.title) === "string") {
+      title = document.createElement("strong");
+      title.className = "title";
+      title.appendChild(document.createTextNode(args.title));
+
+      growl.appendChild(title);
+    }
+
+    growl.appendChild(message); // Append the message to the element
+    growl.className = "growl growl-" + args.type; // Set the class for the growl message
+
+    // Remove the element after some specified time.
+    // We store the timeoutHandle, since we need it later
+    timeoutHandle = setTimeout(function() {
+      growl.parentNode.removeChild(growl);
+    }, args.timeout);
+
+    // If the growl message is clicked, remove it and clear the timeout function
+    growl.onclick = function() {
+      if (timeoutHandle) {
+        clearTimeout(timeoutHandle);
+      }
+      growl.parentNode.removeChild(growl);
+    };
+
+    growlContainer = findOrCreateContainer();
+    growlContainer.appendChild(growl);
+  };
 })(this);
